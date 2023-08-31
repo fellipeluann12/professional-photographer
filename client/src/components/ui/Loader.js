eab2e0f9fedd9dd0c891b5abe1873f1031daf9ff
FELLIPE_LUANN <fellipeluann12@gmail.com>
2023-08-31 12:22:57 -0300

    skeleton loading prototype

--- client/src/components/ui/Loader.js
+++ client/src/components/ui/Loader.js
@@ -1,27 +1,24 @@
-import styled, { keyframes } from 'styled-components';
+import React from 'react';
+import ContentLoader from 'react-content-loader';
 
-const spin = keyframes`
-    0% {
-        rotate: 0deg;
-    }
-    100% {
-        rotate: 360deg;
-    }
-`;
-
-const SLoader = styled.div`
-  display: block;
-  margin: 0 auto;
-  border: 0.2em solid ${({ theme }) => theme.colors.primaryGrey};
-  border-top: 0.2em solid transparent;
-  border-radius: 50%;
-  width: ${({ width }) => (width ? width : '3.28571429rem')};
-  height: ${({ height }) => (height ? height : '3.28571429rem')};
-  animation: ${spin} 0.6s linear infinite;
-`;
-
-const Loader = ({ width, height }) => {
-  return <SLoader width={width} height={height}></SLoader>;
-};
+const Loader = (props) => (
+  <ContentLoader
+    speed={2}
+    width={'35rem'}
+    height={124}
+    viewBox="0 0 476 124"
+    backgroundColor="#b3b3b3"
+    foregroundColor="#828282"
+    {...props}
+  >
+    <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
+    <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
+    <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
+    <rect x="0" y="72" rx="3" ry="3" width="411" height="6" />
+    <circle cx="20" cy="20" r="20" />
+    <rect x="0" y="82" rx="3" ry="3" width="411" height="6" />
+    <rect x="0" y="92" rx="3" ry="3" width="411" height="6" />
+  </ContentLoader>
+);
 
 export default Loader;
